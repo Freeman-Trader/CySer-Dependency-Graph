277f0f9052d40c04649528214d09ece4430d4df1
Freeman Trader <Freeman.Trader@Outlook.com>
2024-02-03 15:18:03 -0800

    Refinded code

--- AST.py
+++ AST.py
@@ -1,94 +1,115 @@
-import ast, copy
+import ast
 import igraph
 
+node_list = list()
 
-# Opens up a file and parses into an AST
+# Redefines functions within the NodeVisitor class to print to file in specific format
+class NodePrinter(ast.NodeVisitor):
+    def __init__(self, output_file):
+        self.output_file = output_file
+
+    def generic_visit(self, node):
+        # print('\n', file=self.output_file)
+        # print('Type: ' + type(node).__name__, file=self.output_file)
+        # print('ID: ' + str(id(node)), file=self.output_file)
+
+        dict_entry = {'Type':type(node).__name__, 'ID':str(id(node))}
+
+        if hasattr(node, 'name'):
+            # print('Name: ' + str(node.name), file=self.output_file)
+            dict_entry['Name'] = str(node.name)
+
+        if hasattr(node, 'body'):
+            # print('Children:', file=self.output_file)
+            temp_array = list()
+
+            for i in node.body:
+                # print(str(id(i)), file=self.output_file)
+                temp_array.append([str(id(i)), type(i).__name__])
+
+            dict_entry['Children'] = temp_array
+
+        if type(node).__name__ == "Expr":
+            if hasattr(node.value.func, "id"):
+                # print('Calling:', file=self.output_file)
+                # print(str(node.value.func.id), file=self.output_file)  
+                dict_entry['Calling'] = str(node.value.func.id)
+            elif hasattr(node.value.func, "attr"):
+                # print('Attr:', file=self.output_file)
+                # print(str(node.value.func.attr), file=self.output_file)  
+                dict_entry['Attr'] = str(node.value.func.attr)
+
+        node_list.append(dict_entry)
+
+        super().generic_visit(node)
+
+
+# Opens up a file and parses into an AST object
 def generate_ast_from_file(filename):
     # Read the source code from the file
     with open(filename, 'r') as file:
         source_code = file.read()
 
     # Generate the AST
-    tree = ast.parse(source_code)
-    return tree
+    ast_object = ast.parse(source_code)
+    return ast_object
 
 
 # Opens file in write mode and then starts the AST traversal
-def write_ast_to_file(ast_object, filename):
-    
+def parse_ast(ast_object, filename):
     with open(filename, 'w') as file:
-        file.write('0: Modules\n')
-        file.write(str(id(ast_object)) + '\n')
-        rTraverseAST(ast_object, file, 1)
+        NodePrinter(file).visit(ast_object)
 
 
-# Recursively goes through the AST and writes the level and element to file 
-def rTraverseAST(ast_object, file, level):
-    if hasattr(ast_object, 'body'):
-        for i in ast_object.body:
-            if(str(type(i)) == "<class 'ast.FunctionDef'>"):
-                file.write(str(level) + ': Function Definition: ' + str(i.name) + '\n')
-                #file.write(str(level) + ': Function Body\n')
-                file.write(str(id(i)) + '\n')
-            elif(str(type(i)) == "<class 'ast.Expr'>"):
-                file.write(str(level) + ': Expr\n')
-                file.write(str(id(i)) + '\n')
-            rTraverseAST(i, file, level+1)
+# def tie_up_loose_statements(graph):
+# Needs work
 
-# Seperated for one it gets to the end of a branch of the AST
-    else:
-        if(str(type(ast_object.value)) == "<class 'ast.FunctionDef'>"):
-            file.write(str(level) + ': Function Definition: ' + str(ast_object.name) + '\n')
-            file.write(str(id(ast_object)) + '\n')
-        elif(str(type(ast_object.value)) == "<class 'ast.Call'>"):
-            file.write(str(level) + ': Function Call: ' + str(ast_object.value.func.id) + '\n')
-            file.write(str(id(ast_object)) + '\n')
-            #if hasattr(ast_object.value, 'args') and ast_object.value.args != None:
-                #file.write(str(level) + ': Function Args\n')
-                #for i in ast_object.value.args:
-                #    file.write(str(level) + ': Value: ' + i.value + '\n')
+# Traverses through node list and adds each node to the graph
+def add_nodes(graph):
+    for index in node_list:
+        graph.add_vertex(name=index['ID'], label=index['Type'])
+
+
+# Traverses through node list and adds each edge relationship to the graph
+def add_edges(graph):
+
+    # Children relationships
+    for index in node_list:
+        if 'Children' in index:
+            for child in index['Children']:
+                graph.add_edge(index['ID'], child[0], color='blue')
+
+    # Calling relationships
+    for index in node_list:
+        if 'Calling' in index:
+            for sIndex in node_list:
+                if 'Name' in sIndex and sIndex['Name'] == index['Calling']:
+                    graph.add_edge(index['ID'], sIndex['ID'], color='green')
 
 
 # Reads file and generates graph from file
-def visualize_ast(filename):
+def visualize_ast():
     newGraph = igraph.Graph(directed=True)
     
-    with open(filename, 'r') as file0:
-        line0 = file0.readline()
-        while line0:
-            parentLevel = int(line0.strip().split(":")[0])
-            parentData = line0.strip().split(":")[1]
-            parentID = file0.readline()
-            newGraph.add_vertex(name=parentID, label=parentData)
-
-            with open('AST.txt', 'r') as file1:
-                file1.seek(file0.tell())
-                for line1 in file1:
-                    childLevel = int(line1.strip().split(":")[0])
-                    childData = line1.strip().split(":")[1]
-                    childID = file1.readline()
-
-                    if (parentLevel == (childLevel - 1)):
-                        newGraph.add_vertex(name=childID, label=childData)
-                        newGraph.add_edge(parentID, childID)
-                    elif(parentLevel == childLevel):
-                        break
-            line0 = file0.readline()
+    add_nodes(newGraph)
+    add_edges(newGraph)
 
     # Deletes the vertices with degree 0
     newGraph.delete_vertices([v.index for v in newGraph.vs if newGraph.degree()[v.index] == 0])
+    # Collects Expr and puts them together
+    # newGraph = tie_up_loose_statements(newGraph)
     # Deletes self-loop edges
-    newGraph.simplify(multiple=False, loops=True)
+    #newGraph.simplify(multiple=False, loops=True)
 
     layout = newGraph.layout('kk')
-    igraph.plot(newGraph, 'ast.png', layout=layout)
+    igraph.plot(newGraph, 'AST.png', layout=layout)
 
 
 # Generates AST
-tree = generate_ast_from_file('test.py')
+ast_object = generate_ast_from_file('test.py')
 
-# Write AST to file
-write_ast_to_file(tree, "AST.txt")
+# Write AST to file and object
+parse_ast(ast_object, "AST.txt")
 
 # Visualize the AST
-visualize_ast("AST.txt")
+visualize_ast()
--- Readme.md
+++ Readme.md
@@ -3,3 +3,20 @@
 ### Mentor: Tashi Stirewal
 
 For the program to work, run pip install igraph on your system and then run python AST.py
+
+added ASTnode visiter
+increase readability via functions
+group loose code together
+
+Nodes
+- classes
+- functions
+    - asynch
+    - def asynch
+- loose statements
+    - global statements
+
+Edges
+- Child-Parent edges
+- Calling edges (different because it could refer to code outside of the program)
+    - Later down the line, trashing edges that call for system defined functions while keeping user defined functions
