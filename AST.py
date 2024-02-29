542cd6165d78af9f4d7c39b84953cc2549cb494e
Freeman Trader <Freeman.Trader@Outlook.com>
2024-02-28 16:30:13 -0800

    Added Location to module and formatted code

--- AST.py
+++ AST.py
@@ -3,6 +3,7 @@ import ast
 import igraph
 
 node_list = list()
+debugfile = open('debug.txt', 'w')
 
 # Redefines functions within the NodeVisitor class to print to file in specific format
 class NodePrinter(ast.NodeVisitor):
@@ -12,16 +13,20 @@ class NodePrinter(ast.NodeVisitor):
     def generic_visit(self, node):
         print('\n', file=self.output_file)
         print('Type: ' + type(node).__name__, file=self.output_file)
-        print('ID: ' + str(id(node)), file=self.output_file)
+        print('Address: ' + str(id(node)), file=self.output_file)
 
-        dict_entry = {'Type':type(node).__name__, 'ID':str(id(node))}
+        dict_entry = {'Type':type(node).__name__, 'Address':str(id(node))}
+
+        if type(node).__name__ == "Module":
+            print('Location: ' + current_file, file=self.output_file)
+            dict_entry['Location'] = current_file
 
         if hasattr(node, 'name'):
             print('Name: ' + str(node.name), file=self.output_file)
             dict_entry['Name'] = str(node.name)
 
         if hasattr(node, 'body'):
-            print('Children:', file=self.output_file)
+            print('Children: ', file=self.output_file)
             temp_array = list()
 
             for i in node.body:
@@ -32,12 +37,34 @@ class NodePrinter(ast.NodeVisitor):
 
         if type(node).__name__ == "Expr":
             if hasattr(node.value.func, "id"):
-                print('Calling:', file=self.output_file)
+                print('Calling: ', file=self.output_file)
                 print(str(node.value.func.id), file=self.output_file)  
                 dict_entry['Calling'] = [str(node.value.func.id)]
             elif hasattr(node.value.func, "attr"):
                 dict_entry['Attr'] = str(node.value.func.attr)
 
+        if type(node).__name__ == "Import":
+            print('Names: ', file=self.output_file)
+            temp_array = list()
+
+            for i in node.names:
+                print(str(id(i)), file=self.output_file)
+                temp_array.append([str(id(i)), type(i).__name__])
+
+            dict_entry['Names'] = temp_array
+
+        if type(node).__name__ == "ImportFrom":
+            print('Module: ' + str(node.module), file=self.output_file)
+            dict_entry['Module'] = str(node.module)
+
+            print('Names: ', file=self.output_file)
+            temp_array = list()
+
+            for i in node.names:
+                print(str(id(i)), file=self.output_file)
+                temp_array.append([str(id(i)), type(i).__name__])
+
+            dict_entry['Names'] = temp_array
 
         node_list.append(dict_entry)
 
@@ -56,13 +83,32 @@ def generate_ast_from_file(filename):
 
 
 # Opens file in write mode and then starts the AST traversal
-def parse_ast(ast_object, filename):
-    with open(filename, 'w') as file:
-        NodePrinter(file).visit(ast_object)
+def save_ast(ast_object):
+    NodePrinter(debugfile).visit(ast_object)
 
 
 # Modifies the node_list so that every node with Expr will have a Loose Statement instead
 def tie_up_loose_statements():
+    # Helper Function for tie_up_loose_statements() that combines 2 nodes calling list
+    def combine_nodes(ID1, ID2):
+        # Finds specificed nodes
+        for node in node_list:
+            if node['Address'] == ID1:
+                node1 = node
+            elif node['Address'] == ID2:
+                node2 = node
+        
+        # Adds node2's list of calls to node1's calls and then deletes node2
+        if node1 and node2:
+            node1['Type'] = 'Loose Statements'
+            node2['Type'] = 'Loose Statements'
+            if 'Calling' in node1 and 'Calling' in node2:
+                node1['Calling'] += node2['Calling']
+            elif 'Calling' in node2:
+                node1['Calling'] = node2['Calling']
+            node_list.remove(node2)
+
+
     # Go through node_list looking for nodes with children
     for node in node_list:
         if 'Children' in node:
@@ -89,33 +135,20 @@ def tie_up_loose_statements():
                 node['Children'].append(loose_expressions[0])
 
 
-# Helper Function for tie_up_loose_statements() that combines 2 nodes calling list
-def combine_nodes(ID1, ID2):
-    # Finds specificed nodes
-    for node in node_list:
-        if node['ID'] == ID1:
-            node1 = node
-        elif node['ID'] == ID2:
-            node2 = node
-    
-    # Adds node2's list of calls to node1's calls and then deletes node2
-    if node1 and node2:
-        node1['Type'] = 'Loose Statements'
-        node2['Type'] = 'Loose Statements'
-        if 'Calling' in node1 and 'Calling' in node2:
-            node1['Calling'] += node2['Calling']
-        elif 'Calling' in node2:
-            node1['Calling'] = node2['Calling']
-        node_list.remove(node2)
+# Utilizes import statements to connect modules
+def link_modules():
+    for index in node_list:
+        if index['Type'] == 'ImportFrom':
+            source = index['Module']
 
 
 # Traverses through node list and adds each node to the graph
 def add_nodes(graph):
     for index in node_list:
         if index['Type'] == 'FunctionDef':
-            graph.add_vertex(name=index['ID'], label='Function - ' + index['Name'], color='blue')
+            graph.add_vertex(name=index['Address'], label='Function - ' + index['Name'], color='blue')
         else:
-            graph.add_vertex(name=index['ID'], label=index['Type'])
+            graph.add_vertex(name=index['Address'], label=index['Type'])
 
 
 # Traverses through node list and adds each edge relationship to the graph
@@ -125,7 +158,7 @@ def add_edges(graph):
     for index in node_list:
         if 'Children' in index:
             for child in index['Children']:
-                graph.add_edge(index['ID'], child[0], color='blue')
+                graph.add_edge(index['Address'], child[0], color='blue')
 
     # Calling relationships
     for index in node_list:
@@ -133,7 +166,7 @@ def add_edges(graph):
             for call in index['Calling']:
                 for sIndex in node_list:
                     if 'Name' in sIndex and sIndex['Name'] == call:
-                        graph.add_edge(index['ID'], sIndex['ID'], color='green')
+                        graph.add_edge(index['Address'], sIndex['Address'], color='green')
 
 
 # Reads file and generates graph from file
@@ -142,6 +175,9 @@ def visualize_ast():
     
     # Collects Expr and puts them together
     tie_up_loose_statements()
+
+    # Links Modules together
+    link_modules()
 
     add_nodes(newGraph)
     add_edges(newGraph)
@@ -153,26 +189,26 @@ def visualize_ast():
 
 
     # My favorite layouts
-    #igraph.plot(newGraph, 'AST.png', layout='reingold_tilford')
+    #igraph.plot(newGraph, 'AST.png', layout='reingold_tilford', vertex_label_size=14, vertex_size=34)
     #igraph.plot(newGraph, 'AST.png', layout='sugiyama')
-    #igraph.plot(newGraph, 'AST.png',vertex_label_size=14, vertex_size=34)
-    igraph.plot(newGraph, 'AST.png', layout='davidson_harel')
-    #igraph.plot(newGraph, 'AST.png', layout='fruchterman_reingold')
+    #igraph.plot(newGraph, 'AST.png', vertex_label_size=14, vertex_size=34)
+    #igraph.plot(newGraph, 'AST.png', layout='davidson_harel')
+    igraph.plot(newGraph, 'AST.png', layout='fruchterman_reingold')
 
 
 # Input name of folder here, folder must be in res
-folder_name = "data" 
+current_file = str()
+ast_object = list()
+folder_name = "data"
 path = os.getcwd() + "\\" + folder_name
 for filename in os.listdir(path):
     # Check for python file extensions
     if filename.endswith('.py'):  
         current_file = os.path.join(path, filename)
-        # Generates AST
-        ast_object = generate_ast_from_file(current_file)
-
-        # Write AST to file and object
-        parse_ast(ast_object, 'AST.txt')
+        # Generates AST then writes AST to file and object
+        save_ast(generate_ast_from_file(current_file))
 
 
 # Visualize the AST
 visualize_ast()
+debugfile.close()
